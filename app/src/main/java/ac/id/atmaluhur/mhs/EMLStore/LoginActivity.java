2f1eadeb79f756156fda3b6d49270937045bc25c
Marcell06 <marcellpat21@gmail.com>
2021-01-11 17:08:15 +0700

    intent

--- app/src/main/java/ac/id/atmaluhur/mhs/EMLStore/LoginActivity.java
+++ app/src/main/java/ac/id/atmaluhur/mhs/EMLStore/LoginActivity.java
@@ -9,6 +9,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile_activity);
+        setContentView(R.layout.home_activity);
     }
 }
--- app/src/main/java/ac/id/atmaluhur/mhs/EMLStore/MainActivity.java
+++ app/src/main/java/ac/id/atmaluhur/mhs/EMLStore/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.view.View;
 import android.content.Intent;
 
 
