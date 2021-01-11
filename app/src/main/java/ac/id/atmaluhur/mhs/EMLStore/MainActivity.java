27772eff10a8d62f6f3dbdb7f325d43c6627841f
Marcell06 <marcellpat21@gmail.com>
2021-01-11 16:43:59 +0700

    intent

--- app/src/main/java/ac/id/atmaluhur/mhs/EMLStore/MainActivity.java
+++ app/src/main/java/ac/id/atmaluhur/mhs/EMLStore/MainActivity.java
@@ -3,12 +3,25 @@ package ac.id.atmaluhur.mhs.EMLStore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mBtn_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
+
+        mBtn_login = (Button)findViewById(R.id.button_login);
+
+        mBtn_login.setOnClickListener(v -> {
+            Intent iLogin = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(iLogin);
+        });
     }
 }
