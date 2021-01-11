61f49e28efa932792d43fc9c4f519a84f7281be4
Marcell06 <marcellpat21@gmail.com>
2021-01-11 14:49:57 +0700

    upload project

---
+++ app/src/main/java/ac/id/atmaluhur/mhs/EMLStore/MainActivity.java
@@ -0,0 +1,14 @@
+package ac.id.atmaluhur.mhs.EMLStore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
